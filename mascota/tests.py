a2321ee4c3a11e079b86788aa84759d93de7525c
Arnaldo Paredes <arnaldop10@gmail.com>
2019-04-13 21:35:06 -0400

    Se agregan pruebas básicas.

--- mascota/tests.py
+++ mascota/tests.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Usuario, Voto
 
-# Create your tests here.
+
+class UsuarioTestCase(TestCase):
+    def setUp(self):
+        self.test_user = Usuario(nickname="prueba", pet_name='test pet')
+        self.test_user.save()
+
+    def test_user_get_string(self):
+        self.assertEquals(str(self.test_user), "test pet")
+
+    def test_get_by_id(self):
+        self.assertEquals(Usuario.objects.get(id=1), self.test_user)
+
+
+class VotoTestCase(TestCase):
+    def setUp(self):
+        self.test_user = Usuario(nickname="prueba", pet_name='test pet')
+        self.test_user.save()
+        self.test_vote = Voto(ip='127.0.0.1', photo_pet=self.test_user)
+        self.test_vote.save()
+
+    def test_get_by_id(self):
+        self.assertEquals(Voto.objects.get(id=1), self.test_vote)
+
+    def test_user_get_string(self):
+        self.assertEquals(str(self.test_vote), "127.0.0.1")
+
+    def tearDown(self):
+        self.test_vote.delete()
