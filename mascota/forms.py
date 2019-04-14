8002f732f3197f25abde519b908ad0c74ed91f00
Arnaldo Paredes <arnaldop10@gmail.com>
2019-04-13 21:34:06 -0400

    Se cambia el formulario con el ModelForm.

--- mascota/forms.py
+++ mascota/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from .models import Usuario
 
+
 class UploadPhotoForm(forms.ModelForm):
-	class Meta:
-		model = Usuario
-		fields = ('nickname', 'pet_name', 'photo_pet')
-		labels = {
-			'nickname': 'Nombre de Usuario',
-			'pet_name': 'Nombre de Mascota',
-			'photo_pet': 'Foto de la Mascota',
-		}
-		widgets = {
-			'nickname': forms.TextInput(attrs={'class': 'form-control'}),
-			'pet_name': forms.TextInput(attrs={'class': 'form-control'}),
-			'photo_pet': forms.FileInput(attrs={'class': 'form-control'}),
-		}
+    class Meta:
+        model = Usuario
+        fields = ('nickname', 'pet_name', 'photo_pet')
+        labels = {
+            'nickname': 'Nombre de Usuario',
+            'pet_name': 'Nombre de Mascota',
+            'photo_pet': 'Foto de la Mascota',
+        }
+        widgets = {
+            'nickname': forms.TextInput(attrs={'class': 'form-control'}),
+            'pet_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'photo_pet': forms.FileInput(attrs={'class': 'form-control'}),
+        }
