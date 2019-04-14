36ec43c9bb968c86cb844f029fadb9255c77b858
Arnaldo Paredes <arnaldop10@gmail.com>
2019-04-13 21:33:22 -0400

    Se agregan los modelos en el admin

--- mascota/admin.py
+++ mascota/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Usuario, Voto
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Voto)
